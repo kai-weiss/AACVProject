bb240f065e380f5a26c29a8c020bb9e7d322a40c
Kai00 <kai.weiss@studmail.htw-aalen.de>
2024-05-29 19:34:08 +0200

    Changing to pretrained model + only train for 10 classes

--- YOLOv8/main.py
+++ YOLOv8/main.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
+# new model from scratch
+# model = YOLO("yolov8n.yaml")
 
-print('test')
-model = YOLO("yolov8n.yaml")
+# pretrained model
+model = YOLO("yolov8n.pt")
 
 # Train model
-results = model.train(data="config.yaml", epochs=1)
+results = model.train(data="config.yaml", epochs=300, patience=50)
 
 # Evaluate model performance
 #results = model.val()
