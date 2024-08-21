4eaefbd7c588f32265d117c127027e9648eedcb4
namrata-jangid <128360488+namrata-jangid@users.noreply.github.com>
2024-08-22 00:27:34 +0530

    Add code for weightedBCE.

---
+++ Weighted BCE Loss/weightedbce_main.py
@@ -0,0 +1,17 @@
+import ultralytics
+from ultralytics import YOLO
+
+def main():
+    # Load the model
+    model = YOLO("yolov8s.pt")
+    
+    # Train the model
+    results = model.train(data="/data/idd_data.yaml", epochs=200, iou=0.5)
+    
+    # Validate the model and print metrics
+    metrics = model.val()
+    print("Validation metrics:")
+    print(metrics)
+
+if __name__ == "__main__":
+    main()
